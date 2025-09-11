77620c68195ba61d88bc8147be79f41c90928527
Samu Makinen <sammkin@hotmail.com>
2025-09-11 15:06:11 +0300

    nelja tehty

---
+++ Week1/A1_T2.py
@@ -0,0 +1,13 @@
+#  Assign variable Name1 with string “John”
+Name1 = "John" #  Print variable Name1
+print(Name1)
+#  Assign variable Name2 with string “Harry”
+Name2 = "Harry"
+# Print variable Name2
+print(Name2)
+# Print variables Name1 and Name2 on the same row separated by a space character
+print(Name1, Name2)
+#   Print “{Name1} is eating ice cream with {Name2}”.
+print(Name1, "is eating ice cream with", Name2)
+#  Print “John and Harry are friends” by using the previously defined variables.
+print(Name1, "and", Name2, "are friends")
