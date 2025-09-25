e118a57ea3de35908c84da48c69cc2af955af9e4
Samu Makinen <sammkin@hotmail.com>
2025-09-25 12:13:44 +0300

    seiska

---
+++ Week2/A2_T7.py
@@ -0,0 +1,9 @@
+#Program starting.
+print("Program starting.")
+#Insert fahrenheits: 50
+Fahr = float(input("Insert fahrenheits: "))
+Cel = (Fahr - 32)/1.8
+#50.0°F is 10.0°C
+print(f"{round(Fahr,1)}°F is {round(Cel,1)}°C")
+#Program ending.
+print("Program ending.")
